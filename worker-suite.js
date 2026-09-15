6be2a57ae43e2a3933207f6a2f5d623c61b97f45
CharlesAAdamsIII <charles.adams1217@gmail.com>
2026-09-15 09:56:50 -0400

    Bootstrap governance schemas before starting workers

--- worker-suite.js
+++ worker-suite.js
@@ -1,16 +1,27 @@
 const {startBusinessProfileWorker}=require('./business-profile-worker');
 const {startAdIntelligenceWorker}=require('./ad-intelligence-worker');
 const {startControlPlaneWorker}=require('./control-plane-worker');
+const adCore=require('./ad-intelligence-core');
+const cp=require('./control-plane-core');
 
-const profiler=startBusinessProfileWorker();
-const advertising=startAdIntelligenceWorker();
-const controlPlane=startControlPlaneWorker();
-require('./worker-monitor');
-
+let profiler=null,advertising=null,controlPlane=null;
+async function start(){
+  const bootstrap=adCore.makePool();
+  if(bootstrap){
+    await adCore.ensureAdSchema(bootstrap);
+    await cp.ensureControlPlaneSchema(bootstrap);
+    await bootstrap.end().catch(()=>{});
+  }
+  profiler=startBusinessProfileWorker();
+  advertising=startAdIntelligenceWorker();
+  controlPlane=startControlPlaneWorker();
+  require('./worker-monitor');
+}
 async function stopWorkers(){
   if(profiler?.stop)await profiler.stop();
   if(advertising?.stop)await advertising.stop();
   if(controlPlane?.stop)await controlPlane.stop();
 }
+start().catch(e=>{console.error('[WORKER SUITE] bootstrap failed',e);process.exit(1)});
 process.on('SIGTERM',stopWorkers);
 process.on('SIGINT',stopWorkers);
