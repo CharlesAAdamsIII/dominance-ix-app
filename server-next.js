38b34464d303275a237192ba11ad36d9f7902b7a
CharlesAAdamsIII <charles.adams1217@gmail.com>
2026-09-15 13:33:41 -0400

    Inject spatial Market Radar visualization layer

--- server-next.js
+++ server-next.js
@@ -14,15 +14,23 @@ function wrappedExpress(...args){
       require('./campaign-workspace-api').attach(router);
       require('./market-intelligence-api').attach(router);
       require('./competitor-intelligence-api').attach(router);
+      const marketRadarPage=(req,res,next)=>{try{
+        const file=fs.readFileSync(path.join(__dirname,'index.html'),'utf8');
+        const spatial='<script src="https://unpkg.com/leaflet.heat@0.2.0/dist/leaflet-heat.js"></script><script src="/market-map-visuals.js?v=1"></script>';
+        res.type('html').send(file.replace('</body>',spatial+'</body>'));
+      }catch(e){next(e)}};
+      router.get('/',pageAuth,marketRadarPage);
+      router.get('/index.html',pageAuth,marketRadarPage);
       router.get('/integrity.html',pageAuth,(req,res,next)=>{try{const file=fs.readFileSync(path.join(__dirname,'integrity.html'),'utf8');res.type('html').send(file.replace('</body>','<script src="/sidebar-collapse.js"></script></body>'))}catch(e){next(e)}});
       router.get('/advertising.html',pageAuth,(req,res,next)=>{try{const file=fs.readFileSync(path.join(__dirname,'advertising.html'),'utf8');res.type('html').send(file.replace('</body>','<script src="/context.js"></script></body>'))}catch(e){next(e)}});
       const stack=app._router?.stack||[];
-      let insertAt=stack.findIndex(layer=>layer?.route?.path==='/campaign.html');
+      let insertAt=stack.findIndex(layer=>layer?.route?.path==='/'||layer?.route?.path==='/index.html');
+      if(insertAt<0)insertAt=stack.findIndex(layer=>layer?.route?.path==='/campaign.html');
       if(insertAt<0)insertAt=stack.findIndex(layer=>layer?.name==='serveStatic');
       if(insertAt<0)insertAt=stack.findIndex(layer=>layer?.route?.path==='*');
       if(insertAt<0)insertAt=stack.length;
       stack.splice(insertAt,0,...router.stack);
-      console.log('[DOMINANCE] Advertising + Control Plane + Campaign + Market + Competitor Intelligence APIs attached');
+      console.log('[DOMINANCE] Advertising + Control Plane + Campaign + Market + Competitor Intelligence APIs attached; Market Radar spatial visualization enabled');
     }catch(e){console.error('[DOMINANCE] API attach failed',e)}
     return originalListen(...listenArgs);
   };
