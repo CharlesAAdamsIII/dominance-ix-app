0d4138ae910adcd2210201792ba6209b5f6c1ccf
CharlesAAdamsIII <charles.adams1217@gmail.com>
2026-09-15 09:55:45 -0400

    Start control plane worker with worker suite

--- worker-suite.js
+++ worker-suite.js
@@ -1,13 +1,16 @@
 const {startBusinessProfileWorker}=require('./business-profile-worker');
 const {startAdIntelligenceWorker}=require('./ad-intelligence-worker');
+const {startControlPlaneWorker}=require('./control-plane-worker');
 
 const profiler=startBusinessProfileWorker();
 const advertising=startAdIntelligenceWorker();
+const controlPlane=startControlPlaneWorker();
 require('./worker-monitor');
 
 async function stopWorkers(){
   if(profiler?.stop)await profiler.stop();
   if(advertising?.stop)await advertising.stop();
+  if(controlPlane?.stop)await controlPlane.stop();
 }
 process.on('SIGTERM',stopWorkers);
 process.on('SIGINT',stopWorkers);
